dfd6f3321c96c73981c42a8b1e5ddd3b7322beb4
Sam Feast <sfeast43@gmail.com>
2024-10-16 21:28:48 +0100

    comments + logging + message formatting + overall standings + improved db queries

--- src/results_cog.py
+++ src/results_cog.py
@@ -28,14 +28,14 @@ class Results(commands.Cog):
         self.bot = bot
 
     # Ping results cog
-    @app_commands.command(description="Ping the reporting cog")
+    @app_commands.command(description="Ping the results cog")
     @app_commands.guilds(discord.Object(id=GUILD_ID))
     async def ping_results(self, interaction: discord.Interaction):
         logger.debug(f"/ping_results used by {interaction.user.id}")
         await interaction.response.send_message("Pong!", ephemeral=True)
 
     # View a players series played
-    @app_commands.command(description="View a players series played")
+    @app_commands.command(description="View a players series record")
     @app_commands.guilds(discord.Object(id=GUILD_ID))
     async def series_played(self, interaction: discord.Interaction, player: discord.User = None):
         logger.debug(f"/series_played used by {interaction.user.id}")
@@ -44,73 +44,73 @@ class Results(commands.Cog):
         if player == None:
             player = interaction.user
 
+        # Get the tier and org of the necessary player
         async with self.bot.pool.acquire() as con:
             res = await con.execute(
-                "SELECT id, name, tier, org FROM players WHERE id = ?", (str(player.id),)
+                "SELECT name, tier, org FROM players WHERE id = ?", (str(player.id),)
             )
             player_info = await res.fetchone()
 
         if player_info == None:
-            logger.warning("Series played failing due to player not found")
-            await interaction.response.send_message("Player not found")
+            logger.warning("/series_played failing as player does not exist")
+            await interaction.response.send_message("Player not found", ephemeral=True)
             return
 
         logger.info("Successfully found player in database")
 
+        # Get the number of 3s series wins and losses
         async with self.bot.pool.acquire() as con:
             res = await con.execute(
-                "SELECT COUNT(*) FROM series_log_3v3 WHERE ? IN(wp1, wp2, wp3)",
-                (player_info["name"],),
+                """SELECT 
+                    SUM(CASE WHEN ? IN(wp1, wp2, wp3) THEN 1 ELSE 0 END) AS wins,
+                    SUM(CASE WHEN ? IN(lp1, lp2, lp3) THEN 1 ELSE 0 END) AS losses
+                FROM series_log_3v3""",
+                (player_info["name"], player_info["name"]),
             )
-            count_win_3v3 = (await res.fetchone())[0]
+            data = await res.fetchone()
+            record_3v3 = f"{data[0]}-{data[1]}"
 
+        # Get the number of 2s series wins and losses
         async with self.bot.pool.acquire() as con:
             res = await con.execute(
-                "SELECT COUNT(*) FROM series_log_3v3 WHERE ? IN(lp1, lp2, lp3)",
-                (player_info["name"],),
+                """SELECT 
+                    SUM(CASE WHEN ? IN(wp1, wp2) THEN 1 ELSE 0 END) AS wins,
+                    SUM(CASE WHEN ? IN(lp1, lp2) THEN 1 ELSE 0 END) AS losses
+                FROM series_log_2v2""",
+                (player_info["name"], player_info["name"]),
             )
-            count_loss_3v3 = (await res.fetchone())[0]
+            data = await res.fetchone()
+            record_2v2 = f"{data[0]}-{data[1]}"
 
+        # Get the number of 1s series wins and losses
         async with self.bot.pool.acquire() as con:
             res = await con.execute(
-                "SELECT COUNT(*) FROM series_log_2v2 WHERE ? IN(wp1, wp2)",
-                (player_info["name"],),
+                """SELECT 
+                    SUM(CASE WHEN wp1 = ? THEN 1 ELSE 0 END) AS wins,
+                    SUM(CASE WHEN lp1 = ? THEN 1 ELSE 0 END) AS losses
+                FROM series_log_1v1""",
+                (player_info["name"], player_info["name"]),
             )
-            count_win_2v2 = (await res.fetchone())[0]
+            data = await res.fetchone()
+            record_1v1 = f"{data[0]}-{data[1]}"
 
-        async with self.bot.pool.acquire() as con:
-            res = await con.execute(
-                "SELECT COUNT(*) FROM series_log_2v2 WHERE ? IN(lp1, lp2)",
-                (player_info["name"],),
-            )
-            count_loss_2v2 = (await res.fetchone())[0]
-
-        async with self.bot.pool.acquire() as con:
-            res = await con.execute(
-                "SELECT COUNT(*) FROM series_log_1v1 WHERE wp1 = ?",
-                (player_info["name"],),
-            )
-            count_win_1v1 = (await res.fetchone())[0]
-
-        async with self.bot.pool.acquire() as con:
-            res = await con.execute(
-                "SELECT COUNT(*) FROM series_log_1v1 WHERE lp1 = ?",
-                (player_info["name"],),
-            )
-            count_loss_1v1 = (await res.fetchone())[0]
-
+        # Send the embed with the org logo as the thumbnail
+        logo_file = ORGS[player_info["org"]]["logo_file"]
+        f = discord.File(f"./assets/{logo_file}", filename="image.png")
         embed = discord.Embed(
             title=f"{player_info['name']}",
-            description=f"{player_info['org']} | {player_info['tier']}",
+            description=f"**{player_info['org']} | {player_info['tier']}**",
+            colour=discord.Colour.from_str(ORGS[player_info["org"]]["colour"]),
         )
-        embed.add_field(name="3v3", value=f"{count_win_3v3}-{count_loss_3v3}", inline=False)
-        embed.add_field(name="2v2", value=f"{count_win_2v2}-{count_loss_2v2}", inline=False)
-        embed.add_field(name="1v1", value=f"{count_win_1v1}-{count_loss_1v1}", inline=False)
+        embed.set_thumbnail(url="attachment://image.png")
+        embed.add_field(name="3v3", value=record_3v3, inline=True)
+        embed.add_field(name="2v2", value=record_2v2, inline=True)
+        embed.add_field(name="1v1", value=record_1v1, inline=True)
 
-        await interaction.response.send_message(embed=embed)
+        await interaction.response.send_message(file=f, embed=embed)
 
     # View the standings for a tier
-    @app_commands.command(description="View the standings for a tier")
+    @app_commands.command(description="View the standings for a specified tier")
     @app_commands.guilds(discord.Object(id=GUILD_ID))
     async def standings(self, interaction: discord.Interaction, tier: str = None):
         logger.debug(f"/standings used by {interaction.user.id}")
@@ -123,56 +123,79 @@ class Results(commands.Cog):
                 )
                 data = await res.fetchone()
                 if data is None:
+                    logger.warning(
+                        f"/standings failing as no tier was supplied by a non playing user"
+                    )
                     await interaction.response.send_message("Tier not found")
                 else:
                     tier = data[0]
 
-        if tier not in TIERS:
-            await interaction.response.send_message("Tier not found")
-            return
-
         org_points = {org: 0 for org in ORGS.keys()}
 
-        async with self.bot.pool.acquire() as con:
-            res = await con.execute(
-                "SELECT winning_org FROM series_log_3v3 WHERE tier = ?",
-                (tier,),
-            )
-            data = await res.fetchall()
+        if tier not in TIERS:
+            if tier == "Overall":
+                logger.info(f"Tier is 'Overall', so getting all series results")
+                # Get the winning org and mode of all the series played
+                async with self.bot.pool.acquire() as con:
+                    res = await con.execute(
+                        """SELECT winning_org, 3 as mode FROM series_log_3v3 UNION ALL
+                            SELECT winning_org, 2 as mode FROM series_log_2v2 UNION ALL
+                            SELECT winning_org, 1 as mode FROM series_log_1v1"""
+                    )
+                    all_series = await res.fetchall()
 
-        for series in data:
-            org_points[series["winning_org"]] += POINTS_3v3
+                # Iterate through the series and add the relevant number of points
+                for series in all_series:
+                    if series["mode"] == 3:
+                        org_points[series["winning_org"]] += POINTS_3v3
+                    elif series["mode"] == 2:
+                        org_points[series["winning_org"]] += POINTS_2v2
+                    elif series["mode"] == 1:
+                        org_points[series["winning_org"]] += POINTS_1v1
+            else:
+                logger.warning(f"/standings used by {interaction.user.id}")
+                await interaction.response.send_message("Invalid tier argument supplied")
+                return
+        else:
+            logger.info(f"Getting all series results for {tier}")
+            # Get the winning org and mode of all the series played in the tier
+            async with self.bot.pool.acquire() as con:
+                res = await con.execute(
+                    """SELECT winning_org, 3 as mode FROM series_log_3v3 WHERE tier = ?
+                        UNION ALL
+                        SELECT winning_org, 2 as mode FROM series_log_2v2 WHERE tier = ?
+                        UNION ALL
+                        SELECT winning_org, 1 as mode FROM series_log_1v1 WHERE tier = ?""",
+                    (tier, tier, tier),
+                )
+                all_series_in_tier = await res.fetchall()
 
-        async with self.bot.pool.acquire() as con:
-            res = await con.execute(
-                "SELECT winning_org FROM series_log_2v2 WHERE tier = ?",
-                (tier,),
-            )
-            data = await res.fetchall()
+            # Iterate through the series and add the relevant number of points
+            for series in all_series_in_tier:
+                if series["mode"] == 3:
+                    org_points[series["winning_org"]] += POINTS_3v3
+                elif series["mode"] == 2:
+                    org_points[series["winning_org"]] += POINTS_2v2
+                elif series["mode"] == 1:
+                    org_points[series["winning_org"]] += POINTS_1v1
 
-        for series in data:
-            org_points[series["winning_org"]] += POINTS_2v2
+        # Round values in the dictionary to avoid floating point errors
+        org_points = {org: round(org_points[org], 1) for org in org_points.keys()}
 
-        async with self.bot.pool.acquire() as con:
-            res = await con.execute(
-                "SELECT winning_org FROM series_log_1v1 WHERE tier = ?",
-                (tier,),
-            )
-            data = await res.fetchall()
-
-        for series in data:
-            org_points[series["winning_org"]] += POINTS_1v1
-
-        org_points = {org: round(org_points[org], 1) for org in ORGS.keys()}
-
+        # Sort the dictionary by values (points)
         ordered_orgs = {
             k: v for k, v in sorted(org_points.items(), key=lambda item: item[1], reverse=True)
         }
 
-        embed = discord.Embed(title=f"{tier} Standings", color=0x1B68BB)
+        logger.info(f"Sending standings embed")
+
+        # Generate and send the standings embed
+        embed = discord.Embed(title=f"{tier} Standings", colour=0x1B68BB)
         position = 1
         for org in ordered_orgs:
-            embed.add_field(name=f"{position}.", value=f"{org}:\t{ordered_orgs[org]}", inline=False)
+            embed.add_field(
+                name=f"{position}.", value=f"{org}:\t{ordered_orgs[org]}", inline=False
+            )
             position += 1
 
         await interaction.response.send_message(embed=embed)
@@ -180,7 +203,9 @@ class Results(commands.Cog):
     @standings.autocomplete("tier")
     async def tier_autocomplete(self, interaction: discord.Interaction, current: str):
         choices = []
-        matched_tiers = [t for t in TIERS.keys() if t.lower().startswith(current.lower())]
+        matched_tiers = [
+            t for t in list(TIERS.keys()) + ["Overall"] if t.lower().startswith(current.lower())
+        ]
         for tier in matched_tiers:
             choices.append(app_commands.Choice(name=tier, value=tier))
 
