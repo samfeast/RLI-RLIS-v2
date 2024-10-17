fe052eed544a32e584ce6e2b4367eb7c26578c58
Sam Feast <sfeast43@gmail.com>
2024-10-17 21:07:52 +0100

    graphical standings instead of old text based standings

--- src/results_cog.py
+++ src/results_cog.py
@@ -96,7 +96,7 @@ class Results(commands.Cog):
 
         # Send the embed with the org logo as the thumbnail
         logo_file = ORGS[player_info["org"]]["logo_file"]
-        f = discord.File(f"./assets/{logo_file}", filename="image.png")
+        f = discord.File(f"./assets/logos/{logo_file}", filename="image.png")
         embed = discord.Embed(
             title=f"{player_info['name']}",
             description=f"**{player_info['org']} | {player_info['tier']}**",
@@ -131,73 +131,11 @@ class Results(commands.Cog):
                 else:
                     tier = data[0]
 
-        org_points = {org: 0 for org in ORGS.keys()}
-
-        if tier not in TIERS:
-            if tier == "Overall":
-                logger.info(f"Tier is 'Overall', so getting all series results")
-                # Get the winning org and mode of all the series played
-                async with self.bot.pool.acquire() as con:
-                    res = await con.execute(
-                        """SELECT winning_org, 3 as mode FROM series_log_3v3 UNION ALL
-                            SELECT winning_org, 2 as mode FROM series_log_2v2 UNION ALL
-                            SELECT winning_org, 1 as mode FROM series_log_1v1"""
-                    )
-                    all_series = await res.fetchall()
-
-                # Iterate through the series and add the relevant number of points
-                for series in all_series:
-                    if series["mode"] == 3:
-                        org_points[series["winning_org"]] += POINTS_3v3
-                    elif series["mode"] == 2:
-                        org_points[series["winning_org"]] += POINTS_2v2
-                    elif series["mode"] == 1:
-                        org_points[series["winning_org"]] += POINTS_1v1
-            else:
-                logger.warning(f"/standings used by {interaction.user.id}")
-                await interaction.response.send_message("Invalid tier argument supplied")
-                return
-        else:
-            logger.info(f"Getting all series results for {tier}")
-            # Get the winning org and mode of all the series played in the tier
-            async with self.bot.pool.acquire() as con:
-                res = await con.execute(
-                    """SELECT winning_org, 3 as mode FROM series_log_3v3 WHERE tier = ?
-                        UNION ALL
-                        SELECT winning_org, 2 as mode FROM series_log_2v2 WHERE tier = ?
-                        UNION ALL
-                        SELECT winning_org, 1 as mode FROM series_log_1v1 WHERE tier = ?""",
-                    (tier, tier, tier),
-                )
-                all_series_in_tier = await res.fetchall()
-
-            # Iterate through the series and add the relevant number of points
-            for series in all_series_in_tier:
-                if series["mode"] == 3:
-                    org_points[series["winning_org"]] += POINTS_3v3
-                elif series["mode"] == 2:
-                    org_points[series["winning_org"]] += POINTS_2v2
-                elif series["mode"] == 1:
-                    org_points[series["winning_org"]] += POINTS_1v1
-
-        # Round values in the dictionary to avoid floating point errors
-        org_points = {org: round(org_points[org], 1) for org in org_points.keys()}
-
-        # Sort the dictionary by values (points)
-        ordered_orgs = {
-            k: v for k, v in sorted(org_points.items(), key=lambda item: item[1], reverse=True)
-        }
-
-        logger.info(f"Sending standings embed")
-
-        # Generate and send the standings embed
-        embed = discord.Embed(title=f"{tier} Standings", colour=0x1B68BB)
-        position = 1
-        for org in ordered_orgs:
-            embed.add_field(name=f"{position}.", value=f"{org}:\t{ordered_orgs[org]}", inline=False)
-            position += 1
-
-        await interaction.response.send_message(embed=embed)
+        f = discord.File(
+            f"../data/graphics/{tier.replace(' ', '_').lower()}.png", filename="image.png"
+        )
+        logger.debug("Ready to send image")
+        await interaction.response.send_message(file=f)
 
     @standings.autocomplete("tier")
     async def tier_autocomplete(self, interaction: discord.Interaction, current: str):
