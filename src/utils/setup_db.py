6533b5ddcb1fa66485ad1af76c589da8cea1c29d
Sam Feast <sfeast43@gmail.com>
2024-10-22 00:58:12 +0100

    much better db setup, constraints, and less tables - STRICT mode

--- src/utils/setup_db.py
+++ src/utils/setup_db.py
@@ -6,39 +6,95 @@ def main():
     def create_blank_db():
         con = sqlite3.connect("../../data/rlis_data.db")
         cur = con.cursor()
-        cur.execute("CREATE TABLE players(id, name, platform, platform_id, tier, org)")
-        cur.execute("CREATE TABLE subs(id, name, platform, platform_id)")
-        cur.execute("CREATE TABLE fixtures(week, tier, org_1, org_2)")
         cur.execute(
-            """CREATE TABLE series_log_1v1(
-            timestamp, game_id, tier, winning_org, losing_org, 
-            games_won_by_loser, played_previously,
-            wp1, lp1, 
-            guid_1, guid_2, guid_3)"""
+            """CREATE TABLE players(
+            id INTEGER, 
+            status TEXT, 
+            name TEXT NOT NULL UNIQUE, 
+            platform TEXT NOT NULL, 
+            platform_id TEXT NOT NULL, 
+            tier TEXT NOT NULL, 
+            org TEXT NOT NULL,
+            PRIMARY KEY(id, status)
+            ) STRICT"""
         )
         cur.execute(
-            """CREATE TABLE series_log_2v2(
-            timestamp, game_id, tier, winning_org, losing_org, 
-            games_won_by_loser, played_previously,
-            wp1, wp2, lp1, lp2,
-            guid_1, guid_2, guid_3)"""
+            """CREATE TABLE fixtures(
+            week INTEGER, 
+            tier TEXT, 
+            org_1 INTEGER, 
+            org_2 INTEGER, 
+            PRIMARY KEY(week, tier, org_1, org_2)
+            ) STRICT"""
         )
         cur.execute(
-            """CREATE TABLE series_log_3v3(
-            timestamp, game_id, tier, winning_org, losing_org, 
-            games_won_by_loser, played_previously,
-            wp1, wp2, wp3, lp1, lp2, lp3, 
-            guid_1, guid_2, guid_3, guid_4, guid_5)"""
+            """CREATE TABLE series_log(
+            timestamp INTEGER NOT NULL, 
+            game_id INTEGER PRIMARY KEY, 
+            tier TEXT NOT NULL, 
+            mode INTEGER NOT NULL, 
+            winning_org TEXT NOT NULL, 
+            losing_org TEXT NOT NULL, 
+            games_won_by_loser INTEGER NOT NULL, 
+            played_previously INTEGER NOT NULL
+            ) STRICT"""
         )
+
+        cur.execute(
+            """CREATE TABLE series_players(
+            game_id INTEGER PRIMARY KEY, 
+            wp1 TEXT, 
+            wp2 TEXT, 
+            wp3 TEXT, 
+            lp1 TEXT, 
+            lp2 TEXT, 
+            lp3 TEXT,
+            FOREIGN KEY(game_id) REFERENCES series_log(game_id) ON DELETE CASCADE,
+            FOREIGN KEY(wp1) REFERENCES players(name),
+            FOREIGN KEY(wp2) REFERENCES players(name),
+            FOREIGN KEY(wp3) REFERENCES players(name),
+            FOREIGN KEY(lp1) REFERENCES players(name),
+            FOREIGN KEY(lp2) REFERENCES players(name),
+            FOREIGN KEY(lp3) REFERENCES players(name)
+            ) STRICT"""
+        )
+
         cur.execute(
             """CREATE TABLE game_stats(
-            guid, url, timestamp, game_id, 
-            overtime_duration, winner_goals, losing_goals)"""
+            guid TEXT PRIMARY KEY, 
+            url TEXT NOT NULL, 
+            timestamp INTEGER NOT NULL, 
+            game_id INTEGER NOT NULL, 
+            duration REAL, 
+            time_in_side_winner REAL, 
+            time_in_side_loser REAL, 
+            overtime_duration REAL, 
+            winner_goals INTEGER, 
+            losing_goals INTEGER, 
+            FOREIGN KEY(game_id) REFERENCES series_log(game_id) ON DELETE CASCADE
+            ) STRICT"""
         )
         cur.execute(
             """CREATE TABLE player_stats(
-            guid, id, game_id, 
-            score, goals, assists, saves, shots)"""
+           guid TEXT NOT NULL, 
+           name TEXT NOT NULL, 
+           game_id INTEGER NOT NULL,
+           goals INTEGER, 
+           assists INTEGER, 
+           saves INTEGER, 
+           shots INTEGER, 
+           score INTEGER, 
+           demos_inflicted INTEGER, 
+           demos_taken INTEGER,
+           car TEXT, 
+           boost_while_ss INTEGER, 
+           time_0_boost REAL, 
+           avg_speed REAL, 
+           dist_travelled INTEGER,
+           PRIMARY KEY(guid, name),
+           FOREIGN KEY(game_id) REFERENCES series_log(game_id) ON DELETE CASCADE,
+           FOREIGN KEY(name) REFERENCES players(name)
+           ) STRICT"""
         )
         con.commit()
 
