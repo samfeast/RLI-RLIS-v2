994770817e1c65b00127d29f1caff7a06e14f4dc
Sam Feast <sfeast43@gmail.com>
2024-10-24 00:35:27 +0100

    determine winner + lots of smaller changes

--- src/get_stats.py
+++ src/get_stats.py
@@ -35,8 +35,81 @@ def max_games_for_mode(mode):
         return MAX_GAMES_1v1
 
 
+def determine_winner(cur, game_id, replay_data):
+    # Try and get the total goals scored by the blue and orange teams
+    try:
+        blue_goals = replay_data["blue"]["stats"]["core"]["goals"]
+    except KeyError:
+        blue_goals = None
+
+    try:
+        orange_goals = replay_data["orange"]["stats"]["core"]["goals"]
+    except KeyError:
+        orange_goals = None
+
+    # If this condition is met the winner cannot be resolved, so return None
+    if blue_goals is None or orange_goals is None or blue_goals == orange_goals:
+        return None, None
+
+    # Get the winning and losing player ids from the replay
+    if blue_goals > orange_goals:
+        game_winners = {
+            (player["id"]["platform"], player["id"]["id"])
+            for player in replay_data["blue"]["players"]
+        }
+        game_losers = {
+            (player["id"]["platform"], player["id"]["id"])
+            for player in replay_data["orange"]["players"]
+        }
+    else:
+        game_winners = {
+            (player["id"]["platform"], player["id"]["id"])
+            for player in replay_data["orange"]["players"]
+        }
+        game_losers = {
+            (player["id"]["platform"], player["id"]["id"])
+            for player in replay_data["blue"]["players"]
+        }
+
+    # Get the orgs, mode, and player names from the series log
+    res = cur.execute(
+        """SELECT L.winning_org, L.losing_org, L.mode,
+        P.wp1, P.wp2, P.wp3, P.lp1, P.lp2, P.lp3
+        FROM series_log AS L JOIN series_players AS P ON L.game_id = P.game_id
+        WHERE L.game_id = ?""",
+        (game_id,),
+    )
+    data = res.fetchone()
+
+    # Get the platforms and platform ids of the players on the winning org (for the series)
+    res = cur.execute(
+        """SELECT platform, platform_id FROM players 
+        WHERE name IN(?, ?, ?)""",
+        (data[3], data[4], data[5]),
+    )
+    # This will never fail since the players in series_players must have an entry in players
+    series_winners = set(res.fetchall())
+
+    # Get the platforms and platform ids of the players on the losing org (for the series)
+    res = cur.execute(
+        """SELECT platform, platform_id FROM players 
+        WHERE name IN(?, ?, ?)""",
+        (data[6], data[7], data[8]),
+    )
+    # This will never fail since the players in series_players must have an entry in players
+    series_losers = set(res.fetchall())
+
+    # Compare the sets to check if whether the series winners won/lost the game
+    # This will always be correct since this function is only called when all the players
+    # in the replay are known
+    if series_winners == game_winners and series_losers == game_losers:
+        return data[0], data[1]
+    else:
+        return data[1], data[0]
+
+
 # Store game and player stats from a specific match
-def store_stats(cur, match_guid, game_id, date, replay_data):
+def store_stats(cur, match_guid, game_id, winning_org, losing_org, date, replay_data, alt_player):
 
     # Parse the date string into a unix timestamp
     timestamp = int(dt.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S%z").timestamp())
@@ -58,15 +131,13 @@ def store_stats(cur, match_guid, game_id, date, replay_data):
 
     try:
         blue_goals = replay_data["blue"]["stats"]["core"]["goals"]
-    except KeyError:
-        blue_goals = None
-
-    try:
         orange_goals = replay_data["orange"]["stats"]["core"]["goals"]
     except KeyError:
+        blue_goals = None
         orange_goals = None
 
-    if blue_goals is not None and orange_goals is not None and blue_goals != orange_goals:
+    # Get win dependent stats if possible
+    if blue_goals is not None and orange_goals is not None:
         if blue_goals > orange_goals:
             winner_goals = blue_goals
             loser_goals = orange_goals
@@ -87,20 +158,17 @@ def store_stats(cur, match_guid, game_id, date, replay_data):
             except KeyError:
                 time_in_side_winner = None
                 time_in_side_loser = None
-    else:
-        winner_goals = None
-        loser_goals = None
-        time_in_side_winner = None
-        time_in_side_loser = None
 
     # Store game stats. Some values may be NULL if they were not included in the response
     cur.execute(
-        "INSERT INTO game_stats VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        "INSERT INTO game_stats VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
         (
             match_guid,
             url,
             timestamp,
             game_id,
+            winning_org,
+            losing_org,
             duration,
             overtime_duration,
             winner_goals,
@@ -110,16 +178,33 @@ def store_stats(cur, match_guid, game_id, date, replay_data):
         ),
     )
 
+    # Combine the blue and orange lists of players
     all_players = replay_data["blue"]["players"] + replay_data["orange"]["players"]
     for player in all_players:
+        # Get the platform and platform id of the player from the response
         platform = player["id"]["platform"]
         platform_id = player["id"]["id"]
 
+        # Get the name of the player from the platform and platform id
         res = cur.execute(
             "SELECT name FROM players WHERE platform = ? AND platform_id = ?",
             (platform, platform_id),
         )
-        name = res.fetchone()[0]
+        name = res.fetchone()
+
+        # If the platform and platform id isn't recognised
+        if name is None:
+            # Check if the 'unknown' platform and platform id match that of the alternate player
+            if alt_player[1][0] == platform and alt_player[1][1] == platform_id:
+                name = alt_player[0]
+            else:
+                # If the platform and platform id isn't recognised at all, skip them
+                logger.warning(
+                    f"Failed to find {platform}:{platform_id} in players table, skipping"
+                )
+                continue
+        else:
+            name = name[0]
 
         try:
             duration = player["end_time"] - player["start_time"]
@@ -215,7 +300,7 @@ def store_stats(cur, match_guid, game_id, date, replay_data):
     logger.info(f"Finished storing game and player stats for {match_guid}")
 
 
-def get(cur, game_id, max_games, existing_guids, start, end, players):
+def get(cur, game_id, max_games, existing_guids, start, end, players, alt_player):
 
     logger.debug(f"Filtering ballchasing between {start} and {end} with {players}")
 
@@ -238,7 +323,7 @@ def get(cur, game_id, max_games, existing_guids, start, end, players):
         # If the guid or date are not present, don't store it (these are required attributes)
         elif match_guid is None or date is None:
             logger.error(
-                f"match_guild or date field not present - unable to save replay with id {replay["id"]}"
+                f"match_guild or date field not present - unable to save replay with id {replay['id']}"
             )
             continue
 
@@ -249,11 +334,19 @@ def get(cur, game_id, max_games, existing_guids, start, end, players):
             logger.error("Unable to store replay - too many replays in filter")
             break
 
+        winning_org, losing_org = determine_winner(cur, game_id, replay_data)
+        # If the winning and losing orgs can't be resolved, dont store it
+        if winning_org is None or losing_org is None:
+            logger.error(f"Unable to resolve winning team - not saving {replay['id']}")
+            continue
+
         logger.info(f"Storing stats for {match_guid}")
-        store_stats(cur, match_guid, game_id, date, replay_data)
+        store_stats(
+            cur, match_guid, game_id, winning_org, losing_org, date, replay_data, alt_player
+        )
 
 
-def from_game_id(cur, game_id, start_timestamp=None, end_timestamp=None):
+def from_game_id(cur, game_id, alt_player, start_timestamp=None, end_timestamp=None):
     # Get the mode, timestamp, played_previously, and players of the game id, and all of the
     # associated replay guids
     res = cur.execute(
@@ -319,10 +412,10 @@ def from_game_id(cur, game_id, start_timestamp=None, end_timestamp=None):
     )
     players = res.fetchall()
 
-    get(cur, game_id, max_games, existing_guids, start, end, players)
+    get(cur, game_id, max_games, existing_guids, start, end, players, alt_player)
 
 
-def from_replay_id(cur, game_id, replay_id):
+def from_replay_id(cur, game_id, replay_id, winning_org, losing_org, alt_player):
     res = cur.execute(
         """SELECT L.mode, L.games_won_by_loser, S.guid 
         FROM 
@@ -375,7 +468,7 @@ def from_replay_id(cur, game_id, replay_id):
         return
 
     logger.info(f"Storing stats for {match_guid}")
-    store_stats(cur, match_guid, game_id, date, replay_data)
+    store_stats(cur, match_guid, game_id, winning_org, losing_org, date, replay_data, alt_player)
 
 
 def main():
@@ -401,18 +494,20 @@ def main():
 
     logger.info(f"Popped entry from stats stack - {data}")
 
-    # If a replay id is included, request that
-    if data[2] is not None:
+    alt_player = (data[7], (data[8], data[9]))
+
+    # If a replay id and winning/losing orgs are included, search for that
+    if data[2] is not None and data[5] is not None and data[6] is not None:
         logger.info("Getting replay from replay id")
-        from_replay_id(cur, data[1], data[2])
+        from_replay_id(cur, data[1], data[2], data[5], data[6], alt_player)
     # If there is no replay id but timestamps are included, search using them
     elif data[3] is not None and data[4] is not None:
         logger.info("Getting replay from game id with specified times")
-        from_game_id(cur, data[1], data[3], data[4])
+        from_game_id(cur, data[1], alt_player, start_timestamp=data[3], end_timestamp=data[4])
     # If only a game id is included, infer times
     else:
         logger.info("Getting replay from game id")
-        from_game_id(cur, data[1])
+        from_game_id(cur, data[1], alt_player)
 
     # Delete the item that's been popped off the stack
     cur.execute(
@@ -420,11 +515,12 @@ def main():
         WHERE priority = (SELECT MAX(priority) FROM stats_stack)"""
     )
 
-    # Update the number of replays stored in the series log
+    # Update the number of replays stored and unpublish series in series log
     cur.execute(
         """UPDATE series_log 
-        SET replays_stored = (SELECT COUNT(guid) FROM game_stats WHERE game_id = ?)""",
-        (data[1],),
+        SET replays_stored = (SELECT COUNT(guid) FROM game_stats WHERE game_id = ?),
+        published = 0 WHERE game_id = ?""",
+        (data[1], data[1]),
     )
 
     con.commit()
